a97bda66e2df2e486393f0b8ef6f44f9b7f513c8
Jeremy Poulin <jpoulin@redhat.com>
2018-03-27 15:16:22 -0400

    Updated dependecies to prevent circular appends

--- install/dependencies.sh
+++ install/dependencies.sh
@@ -11,7 +11,7 @@ echo "Enter the desired OpenShift version (e.g. v3.6.1) [default LATEST]:";
 read openshift_version;
 
 # Download Specified OpenShift client
-if [ -z "$openshift_version" ] 
+if [ -z "$openshift_version" ]
 then
   openshift_version="$OPENSHIFT__LATEST"
 else
@@ -29,8 +29,37 @@ sudo systemctl start docker;
 sudo systemctl start firewalld;
 
 # Allow OpenShift docker registry
-echo "INSECURE_REGISTRY='--insecure-registry 172.30.0.0/16'" | sudo tee -a /etc/sysconfig/docker;
-echo "{ \"mtu\": 1400 }" | sudo tee /etc/docker/daemon.json;
+echo "Adding OpenShift insecure registry to docker"
+if grep -xqsE "^[[:space:]]*INSECURE_REGISTRY='--insecure-registry 172.30.0.0/16'" /etc/sysconfig/docker
+then
+  echo "Registry already enabled"
+else
+  if grep -xqsE "# INSECURE_REGISTRY='--insecure-registry'" /etc/sysconfig/docker
+  then
+    echo "Adding insecure registery to /etc/sysconfig/docker"
+    sed -E "/# INSECURE_REGISTRY='--insecure-registry'/a INSECURE_REGISTRY='--insecure-registry 172.30.0.0/16'" /etc/sysconfig/docker | sudo tee 1> /dev/null /etc/sysconfig/docker
+  else
+    echo "Appending insecure registry to end of /etc/sysconfig/docker"
+    echo "INSECURE_REGISTRY='--insecure-registry 172.30.0.0/16'" | sudo tee 1> /dev/null -a /etc/sysconfig/docker
+  fi
+fi
+
+# Reduce the MTU for docker so that packets don't get dropped
+echo "Reducing MTU to match CI-RHOS environment"
+if grep -qsE "[\'\"]mtu[\'\"]\s*:\s*[\'\"]?[0-9]+[\'\"]?" /etc/docker/daemon.json
+then
+  echo "Downgrading existing MTU configuration to 1400 in /etc/docker/daemon.json"
+  sed -E "s/[\'\"]mtu[\'\"]\s*:\s*[\'\"]?[0-9]+[\'\"]?/\"mtu\": 1400/" /etc/docker/daemon.json | sudo tee 1> /dev/null /etc/docker/daemon.json
+else
+  if grep -qzsE "\{.*:.*\}" /etc/docker/daemon.json
+  then
+    echo "Adding MTU configuration to /etc/docker/daemon.json after the opening curly brace"
+    sed -E "s/\{/\{\n  \"mtu\"\: 1400\,/" /etc/docker/daemon.json | sudo tee 1> /dev/null /etc/docker/daemon.json
+  else
+    echo "Create a new /etc/docker/daemon.json to set MTU to 1400"
+    echo "{ \"mtu\": 1400 }" | sudo tee 1> /dev/null /etc/docker/daemon.json
+  fi
+fi
 
 # Open up ports for connections to docker
 sudo firewall-cmd --permanent --new-zone dockerc;
