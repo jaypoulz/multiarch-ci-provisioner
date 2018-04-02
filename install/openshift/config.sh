26f6773502b5cf40aefaf10b21acc823e3696cff
Jeremy Poulin <jpoulin@redhat.com>
2018-04-02 13:56:51 -0400

    Updating OpenShift release

--- install/openshift/config.sh
+++ install/openshift/config.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #
 # openshift/config.sh
+OPENSHIFT__v3_9_0="https://github.com/openshift/origin/releases/download/v3.9.0/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit.tar.gz"
 OPENSHIFT__v3_7_2="https://github.com/openshift/origin/releases/download/v3.7.2/openshift-origin-client-tools-v3.7.2-282e43f-linux-64bit.tar.gz"
 OPENSHIFT__v3_7_1="https://github.com/openshift/origin/releases/download/v3.7.1/openshift-origin-client-tools-v3.7.1-ab0f056-linux-64bit.tar.gz"
 OPENSHIFT__v3_7_0="https://github.com/openshift/origin/releases/download/v3.7.0/openshift-origin-client-tools-v3.7.0-7ed6862-linux-64bit.tar.gz"
 OPENSHIFT__v3_6_1="https://github.com/openshift/origin/releases/download/v3.6.1/openshift-origin-client-tools-v3.6.1-008f2d5-linux-64bit.tar.gz"
 OPENSHIFT__v3_6_0="https://github.com/openshift/origin/releases/download/v3.6.0/openshift-origin-client-tools-v3.6.0-c4dd4cf-linux-64bit.tar.gz"
 
-OPENSHIFT__LATEST="OPENSHIFT__v3_7_2"
+OPENSHIFT__LATEST="OPENSHIFT__v3_9_0"
