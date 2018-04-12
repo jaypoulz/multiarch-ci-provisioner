1124aaa47272315bc9c6aab7b6989168996ef445
Jeremy Poulin <jpoulin@redhat.com>
2018-04-12 14:29:59 -0400

    Added script to help install Jenkins secrets via OpenShift sync

--- setup/openshift/provisioner.sh
+++ setup/openshift/provisioner.sh
@@ -2,12 +2,19 @@
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 oc login -u developer
-oc create -f ../templates/provisioner.yml
 
+# Install Jenkins secrets
+bash openshift/provisioner/jenkins-secrets.sh
+
+# Create the template and its configuration secrets
+oc create -f ../templates/provisioner.yml
 oc create secret generic beaker --from-file ../images/provisioner/secrets/beaker/client.conf
 oc create secret generic krb5 --from-file ../images/provisioner/secrets/krb5/krb5.conf
 
+# Create the actual provisioner
 oc new-app provisioner-builder
+
+# Promote Jenkins Service Account to privileged for external Jenkins setups
 oc login -u system:admin
 oc adm policy add-scc-to-user privileged system:serviceaccount:redhat-multiarch-qe:jenkins
 oc login -u developer
