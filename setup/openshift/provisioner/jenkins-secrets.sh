17bb813d2b1f1f6fac3210bd01f0369ab7caec4d
Jeremy Poulin <jpoulin@redhat.com>
2018-04-18 16:27:06 -0400

    Fixed the problem with gitignore blocking the install of the plugin script

--- setup/openshift/provisioner/jenkins-secrets.sh
+++ setup/openshift/provisioner/jenkins-secrets.sh
@@ -7,31 +7,31 @@ echo "  - SSH Public Key"
 echo "  - Kerberos Keytab"
 echo "Would like like to install beaker secrets? [y/N]"
 read install_secrets
-if [ "$install_secrets"= "y" ]
+if [ "$install_secrets"="y" ]
 then
   # Install SSH Private Key
   echo "Please enter the path of the SSH Private Key:"
-  read ssh_priv_key
+  read -e ssh_priv_key
   # Create the secret
-  oc create secret generic SSHPRIVKEY --from-file=filename="${ssh_priv_key}"
+  oc create secret generic sshprivkey --from-file=filename="${ssh_priv_key}"
   # Add label to mark that it should be synced.
-  oc label secret SSHPRIVKEY credential.sync.jenkins.openshift.io=true
+  oc label secret sshprivkey credential.sync.jenkins.openshift.io=true
   
   # Install SSH Public Key
   echo "Please enter the path of the SSH Public Key:"
-  read ssh_pub_key
+  read -e ssh_pub_key
   # Create the secret
-  oc create secret generic SSHPUBKEY --from-file=filename="${ssh_pub_key}"
+  oc create secret generic sshpubkey --from-file=filename="${ssh_pub_key}"
   # Add label to mark that it should be synced.
-  oc label secret SSHPUBKEY credential.sync.jenkins.openshift.io=true
+  oc label secret sshpubkey credential.sync.jenkins.openshift.io=true
 
   # Install Kerberos Keytab
   echo "Please enter the path of the Kerberos Keytab:"
-  read keberos_keytab 
+  read -e kerberos_keytab 
   # Create the secret
-  oc create secret generic KEYTAB --from-file=filename="${kerberos_keytab}"
+  oc create secret generic keytab --from-file=filename="${kerberos_keytab}"
   # Add label to mark that it should be synced.
-  oc label secret KEYTAB credential.sync.jenkins.openshift.io=true
+  oc label secret keytab credential.sync.jenkins.openshift.io=true
 else
   exit 0
 fi
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+oc cluster down
+sudo rm -rf ~/origin
+sudo rm -rf /var/lib/origin
