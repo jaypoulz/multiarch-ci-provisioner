60e52f868f8f9262a6fdefa09fed269367e6e881
Jeremy Poulin <jpoulin@redhat.com>
2018-04-19 16:58:25 -0400

    Adding init.groovy to Jenkins master. I hope that it will work because the plugin to create the developer-admin user will already have run.

---
+++ s2i/jenkins/master/configuration/init.groovy
@@ -0,0 +1,30 @@
+def username = 'developer-admin'
+def credentialName = 'jenkins-slave-credentials'
+def credentialDesc = 'Credentials used by multiarch-ci-libraries to connect provisioned hosts to Jenkins'
+
+import hudson.model.User
+import jenkins.security.ApiTokenProperty
+import hudson.util.Secret
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.domains.*
+import com.cloudbees.plugins.credentials.impl.*
+
+// Get the actual token
+u = User.get(username)
+tokProp =  u.getProperty(ApiTokenProperty.class)
+token = tokProp.getApiTokenInsecure()
+
+// Get system credentials
+domain = Domain.global()
+store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
+
+// Add the new credentials to the global scope
+credentials = new UsernamePasswordCredentialsImpl(
+  CredentialsScope.GLOBAL,
+  credentialName,
+  credentialDesc,
+  username,
+  token
+)
+
+store.addCredentials(domain, credentials)
