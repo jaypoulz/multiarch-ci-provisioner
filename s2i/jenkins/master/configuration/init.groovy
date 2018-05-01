0b8e3d11c3e53f6454b2273e15a497804af504ef
Jeremy Poulin <jpoulin@redhat.com>
2018-05-01 15:22:47 -0400

    Adding additional configuration to Jenkins environment

--- s2i/jenkins/master/configuration/init.groovy
+++ s2i/jenkins/master/configuration/init.groovy
@@ -1,12 +1,23 @@
 import hudson.model.*
 import jenkins.model.*
-import jenkins.security.ApiTokenProperty
+import jenkins.security.*
+import jenkins.security.csrf.DefaultCrumbIssuer
 import hudson.util.Secret
 import com.cloudbees.plugins.credentials.*
 import com.cloudbees.plugins.credentials.domains.*
 import com.cloudbees.plugins.credentials.impl.*
 
 jenkins = Jenkins.instance
+def logger = Logger.getLogger("")
+
+logger.info("Enabling CSRF Protection")
+jenkins.setCrumbIssuer(new DefaultCrumbIssuer(true))
+
+logger.info("Disabling deprecated agent protocols (only JNLP4 is enabled)")
+Set<String> agentProtocolsList = ['JNLP4-connect']
+jenkins.setAgentProtocols(agentProtocolsList)
+
+jenkins.save()
 
 def username = 'developer-admin'
 def credentialName = 'jenkins-slave-credentials'
@@ -19,7 +30,7 @@ token = tokProp.getApiTokenInsecure()
 
 // Get system credentials
 domain = Domain.global()
-store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
+store = jenkins.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
 
 // Add the new credentials to the global scope
 credentials = new UsernamePasswordCredentialsImpl(
@@ -30,4 +41,9 @@ credentials = new UsernamePasswordCredentialsImpl(
   token
 )
 
+logger.info("Adding developer-admin token as secret so that slaves can authenticate")
 store.addCredentials(domain, credentials)
+
+logger.info("Setting Time Zone to be EST")
+System.setProperty('org.apache.commons.jelly.tags.fmt.timeZone', 'America/New_York')
+
